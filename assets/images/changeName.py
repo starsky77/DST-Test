fe13880c6faaf10c0f3cff5505a3e6b03d98caf6
Ziqi Wen <399680958@qq.com>
2023-09-18 13:42:09 -0400

    Init

---
+++ assets/images/changeName.py
@@ -0,0 +1,35 @@
+import os
+
+def rename_files_in_folder(folder_path):
+    # List all files in the directory
+    filenames = os.listdir(folder_path)
+    
+    # Shuffle the filenames to change their order
+    # import random
+    # random.shuffle(filenames)
+    
+    # Counter for the new filenames
+    counter = 1
+    
+    for filename in filenames:
+        # Get file extension
+        file_extension = os.path.splitext(filename)[1]
+        
+        # Construct new filename
+        new_filename = f"{counter}{file_extension}"
+        
+        # Full path to the original and new files
+        original_file = os.path.join(folder_path, filename)
+        new_file = os.path.join(folder_path, new_filename)
+        
+        # Rename the file
+        os.rename(original_file, new_file)
+        
+        # Increment the counter
+        counter += 1
+
+# Specify the folder path where your images are stored
+folder_path = "C:/Users/39968/Documents/GitHub/TriviaGame/assets/images/set2"
+
+# Call the function
+rename_files_in_folder(folder_path)
